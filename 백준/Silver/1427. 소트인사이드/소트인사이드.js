7a853ca597c9ff6b4cc95add1aba5a16733c3cb5
JS. Kim <75792069+Junseok3608@users.noreply.github.com>
2023-01-11 22:25:33 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("")
+  .map(Number)
+  .sort((a, b) => b - a);
+console.log(input.join(""));
