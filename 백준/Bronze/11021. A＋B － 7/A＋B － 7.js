ec464e74b1c53148c5dffa6d3d7201adf6d2f154
JS. Kim <75792069+Junseok3608@users.noreply.github.com>
2023-01-11 22:15:54 +0900

    [Bronze V] Title: A+B - 7, Time: 120 ms, Memory: 9356 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,12 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+const counter = Number(input[0]);
+let result = "";
+
+for (let i = 1; i <= counter; i++) {
+  const nums = input[i].split(" ").map(Number);
+  result += `Case #${i}: ${nums[0] + nums[1]}\n`;
+}
+
+console.log(result);
+
+
