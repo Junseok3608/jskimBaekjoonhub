b87fd5cc229b10ddf62f63e484edd741c846df60
JS. Kim <75792069+Junseok3608@users.noreply.github.com>
2023-01-13 09:03:11 +0900

    [Silver V] Title: 단어 정렬, Time: 1400 ms, Memory: 24084 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+input.shift();
+
+input = Array.from(new Set(input));
+let result = [];
+let sort = input.sort((a, b) => {
+  if (a.length !== b.length) {
+    return a.length - b.length;
+  }
+});
+for (let i = 1; i <= sort[sort.length - 1].length; i++) {
+  let tm = sort.filter((el) => el.length === i);
+  result.push(...tm.sort());
+}
+for (let j of result) {
+  console.log(j);
+}
