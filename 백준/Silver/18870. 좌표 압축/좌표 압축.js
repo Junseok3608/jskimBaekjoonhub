ef29b80bf4e2188e115299cef4f87e82559c8776
JS. Kim <75792069+Junseok3608@users.noreply.github.com>
2023-01-16 17:10:16 +0900

    [Silver II] Title: 좌표 압축, Time: 2348 ms, Memory: 340212 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const N = +input.shift();
+
+const setArr = input[0]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+const set = new Set(setArr);
+const map = new Map();
+
+[...set].forEach((item, index) => {
+  map.set(item, index);
+});
+
+let result = "";
+input[0].split(" ").forEach((item, index) => {
+  result += map.get(+item) + " ";
+});
+console.log(result);
