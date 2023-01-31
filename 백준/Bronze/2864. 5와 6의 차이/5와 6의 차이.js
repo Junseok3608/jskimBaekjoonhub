19b577b36eb6856be834c360324e09421536e704
JS. Kim <75792069+Junseok3608@users.noreply.github.com>
2023-01-31 09:32:20 +0900

    [Bronze II] Title: 5와 6의 차이, Time: 116 ms, Memory: 9320 KB -BaekjoonHub

--- 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.js
+++ 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.js
@@ -3,6 +3,8 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.text";
 let input = fs.readFileSync(filePath).toString().trim();
 const toFive = input.replace(/6/g, "5").split(" ").map(Number);
 const toSix = input.replace(/5/g, "6").split(" ").map(Number);
+const resultMin = toFive[0] + toFive[1];
+const resultMax = toSix[0] + toSix[1];
+let result = `${resultMin} ${resultMax}`;
 
-console.log(toFive[0] + toFive[1]);
-console.log(toSix[0] + toSix[1]);
+console.log(result);
