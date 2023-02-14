a7e0ebfe9c0374f50b2bc36d7434faf1e62e0bea
JS. Kim <75792069+Junseok3608@users.noreply.github.com>
2023-02-14 19:43:41 +0900

    [Silver I] Title: 회의실 배정, Time: 396 ms, Memory: 43876 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.text";
+const [n, ...input] = fs.readFileSync(filePath).toString().trim().split("\n");
+let ans = 0;
+const arr = input
+  .map((a) => a.split(" ").map(Number))
+  .sort((a, b) => {
+    if (a[1] === b[1]) {
+      return a[0] - b[0];
+    } else {
+      return a[1] - b[1];
+    }
+  });
+
+let fin = 0;
+arr.forEach((t) => {
+  if (t[0] >= fin) {
+    ans++;
+    fin = t[1];
+  }
+});
+console.log(ans);
